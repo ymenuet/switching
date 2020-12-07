f5b0fb06624b83df1b33241545dd120fdb8f9e57
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-12-06 21:43:46 -0500

    create docker config files

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ import { ok } from 'assert'
 // import { errorMonitor } from 'nodemailer/lib/mailer'
 const __dirname = path.resolve()
 
-dotenv.config()
+dotenv.config({ path: '../.env' })
 
 connectDB()
 
@@ -30,8 +30,8 @@ app.get('/', (req, res) => {
 
 const sslServer = https.createServer(
   {
-    key: fs.readFileSync(path.join(__dirname, 'backend', 'cert', 'key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'backend', 'cert', 'cert.pem')),
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
   },
   app
 )
