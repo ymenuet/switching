770a9ac71fb5bff5e2bdcf3eb39475ea21b40c4a
Nicolas Fraisse <ni.fraisse@gmail.com>
2020-12-07 10:23:01 -0500

    create dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,7 @@ import Formations from './pages/Formations'
 import Formation from './pages/Formation'
 import Login from './pages/platform/Login'
 import Register from './pages/platform/Register'
+import Dashboard from './pages/platform/Dashboard'
 import Profile from './pages/platform/Profile'
 import UserList from './pages/admin/UserList'
 import UserEdit from './pages/admin/UserEdit'
@@ -14,7 +15,6 @@ import FormationEdit from './pages/admin/FormationEdit'
 import Purchase from './pages/Purchase'
 import NotFound from './pages/404'
 import Header from './components/navigation/Header'
-import Container from './components/Container'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
 const stripePromise = loadStripe(process.env.REACT_APP_STRIPE_PK!)
@@ -37,6 +37,7 @@ const App = () => {
         <Route path='/404' component={NotFound} />
         <Route path='/about' component={About} />
         <Route path='/formations' exact component={Formations} />
+        <Route path='/mes-cours' exact component={Dashboard} />
         <Route path='/formations/:id' component={Formation} />
         <Route
           path='/purchase'
---
+++ frontend/src/pages/platform/Dashboard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1 className='py-4 text-center'>Mes cours</h1>
+    </div>
+  )
+}
+
+export default Dashboard
